2ad5dab73b33c7722ee67f597b019290730f0d22
Tony John <tony.john@iselect.com.au>
2019-11-10 02:36:55 +1100

    Add seed data

---
+++ tests/seed/data.ts
@@ -0,0 +1,15 @@
+import { Ad } from '../../src/models/ad';
+import { Customer } from '../../src/models/customer';
+
+export const ads: Ad[] = [
+  {name: 'Classic Ad', description: 'Offers the most basic level of advertisement', price: 269.99},
+  {name: 'Stand out Ad', description: 'Allows advertisers to use a company logo and use a longer presentation text', price: 322.99},
+  {name: 'Premium Ad', description: 'Same benefits as Standout Ad, but also puts the advertisement at the top of the results, allowing higher visibility', price: 394.99},
+];
+
+export const customers: Customer[] = [
+  {name: 'Default'},
+  {name: 'SecondBite'},
+  {name: 'Axil Coffee Roasters'},
+  {name: 'Myer'},
+]; 
